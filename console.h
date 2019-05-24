135f551261b14940f57ddab2f665028651caee11
Đinh Hữu Kiện <46819741+tigertmjv@users.noreply.github.com>
2019-05-24 20:17:47 +0700

    Add files via upload

---
+++ console.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include<iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+
+using namespace std;
+
+void appMenu();
+void clrscr();
+void gotoXY(int x ,int y);
